2bfca21d6075444820c7d4857a20085f1b3c4b10
francisco-duo <francisco.duo2000@gmail.com>
2023-06-14 17:53:20 -0300

    contacts

--- apps/contact/views/contact_views.py
+++ apps/contact/views/contact_views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from contact.models import Contact
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request, ):
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
     return render(
         request,
         'contact/index.html',
+        {
+            'contacts': contacts
+        }
+    )
+
+
+def contact(request, contact_id):
+    single_contacts = get_object_or_404(
+        Contact.objects, pk=contact_id, show=True
+        )
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': single_contacts
+        }
     )
