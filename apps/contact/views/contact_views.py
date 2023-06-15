4a58bef9eaa3c2aa4695b8e121ca594c3da785c1
Francisco Duó <francisco.duo2000@gmail.com>
2023-06-14 21:44:44 -0300

    search view

--- apps/contact/views/contact_views.py
+++ apps/contact/views/contact_views.py
@@ -1,6 +1,6 @@
 from contact.models import Contact
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, render, redirect
 
 
 def index(request, ):
@@ -25,5 +25,30 @@ def contact(request, contact_id):
         'contact/contact.html',
         {
             'contact': single_contacts
+        }
+    )
+
+
+def search(request, ):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    return render(
+        request,
+        'contact/index.html',
+        {
+            'contacts': contacts
         }
     )
